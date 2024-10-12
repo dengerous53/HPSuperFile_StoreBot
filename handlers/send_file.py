80ff00f2f5d10223ddb140bfc1b08074682331a4
Dengerous <99312935+dengerous53@users.noreply.github.com>
2024-10-12 23:33:06 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -8,8 +8,8 @@ from handlers.helpers import str_to_b64
 
 async def reply_forward(message: Message, file_id: int):
     try:
-        dream = await message.reply_text(
-            f"**Files will be Deleted After 01 min ⏰**\n",
+        await message.reply_text(
+            f"**Files will be Deleted After 30 min ⏰**\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -32,11 +32,8 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 60))
+    asyncio.create_task(delete_after_delay(sent_message, 1800))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
     await message.delete()
-    
-    await asyncio.sleep(2)
-    await dream.edit_text("<b>Thank For Using Me...</b>")
