743e16eb9dfc8431ec82b26e2e484134d5d45045
Dengerous <99312935+dengerous53@users.noreply.github.com>
2024-10-13 00:31:05 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -2,19 +2,26 @@ import asyncio
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, MessageDeleteForbidden
 from handlers.helpers import str_to_b64
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 async def reply_forward(message: Message, file_id: int):
     try:
-        await message.reply_text(
-            f"**Files will be Deleted After 01 min ⏰**",
+        reply = await message.reply_text(
+            f"**Files will be Deleted After 01 min **",
             disable_web_page_preview=True,
             quote=True
         )
+        return reply
     except FloodWait as e:
+        logging.warning(f"FloodWait: {e}")
         await asyncio.sleep(e.value)
-        await reply_forward(message, file_id)
+        return await reply_forward(message, file_id)
+    except Exception as e:
+        logging.error(f"Error: {e}")
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -31,16 +38,31 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
                 message_ids=file_id
             )
     except FloodWait as e:
+        logging.warning(f"FloodWait: {e}")
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
+    except Exception as e:
+        logging.error(f"Error: {e}")
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id)
-    reply_message = await reply_forward(sent_message, file_id)
-    reply_message_text = "Thanks for using me"
-    asyncio.create_task(delete_after_delay(reply_message, sent_message, 60, reply_message_text))
+    try:
+        sent_message = await media_forward(bot, user_id, file_id)
+        reply_message = await reply_forward(sent_message, file_id)
+        reply_message_text = "Thanks for using me "
+        asyncio.create_task(delete_after_delay(reply_message, sent_message, 60, reply_message_text))
+    except Exception as e:
+        logging.error(f"Error: {e}")
 
 async def delete_after_delay(message, file_message, delay, reply_message_text):
-    await asyncio.sleep(delay)
-    await file_message.delete()
-    await message.edit_text(reply_message_text)
+    try:
+        await asyncio.sleep(delay)
+        await file_message.delete()
+        if message:
+            try:
+                await message.edit_text(reply_message_text)
+            except MessageDeleteForbidden:
+                logging.warning("Message deletion forbidden")
+        else:
+            logging.warning("Message not found")
+    except Exception as e:
+        logging.error(f"Error: {e}")
